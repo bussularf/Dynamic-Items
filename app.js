796a3fbc81ea788b8de333877fa58a06a00fb9f7
Fernanda de Jesus <fernandabussular@gmail.com>
2024-11-15 03:50:55 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { items: [] });
+});
+
+app.get('/generate-items', (req, res) => {
+  const quantity = parseInt(req.query.quantity) || 0;
+  const items = Array.from({ length: quantity }, (_, i) => `Item ${i + 1}`);
+  res.json(items);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
